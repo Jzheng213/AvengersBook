3886233ddaab909ee5e27002cff513f65ee605f0
Jack Zheng <jzheng213@gmail.com>
2018-03-09 12:38:47 -0500

    Add cover picture to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308211337) do
+ActiveRecord::Schema.define(version: 20180309173242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20180308211337) do
     t.string "profile_pic_content_type"
     t.integer "profile_pic_file_size"
     t.datetime "profile_pic_updated_at"
+    t.string "cover_pic_file_name"
+    t.string "cover_pic_content_type"
+    t.integer "cover_pic_file_size"
+    t.datetime "cover_pic_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
