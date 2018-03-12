fa6f1bb6719449e3c78909df305268052df5e9b4
Jack Zheng <jzheng213@gmail.com>
2018-03-12 14:37:02 -0400

    Add paperclip config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,15 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['s3_bucket'],
+      :access_key_id => ENV['s3_access_key_id'],
+      :secret_access_key => ENV['s3_secret_access_key'],
+      :s3_region => ENV['s3_region']
+    }
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
